00724397c03ce1108df0898822bbc3fda9ba87c6
Smietan94 <krzysztof.smietana@rebilly.com>
2024-08-30 16:51:18 +0200

    add new stuff

--- instruments-config.js
+++ instruments-config.js
@@ -11,6 +11,9 @@ RebillyInstruments.mount({
         buttonColorText: '#ffffff',
         fontFamily: 'Trebuchet MS, sans-serif' // Website font family
     },
+    features: {
+        hideConfirmation: true,
+    },
     paymentInstruments: {
         address: {
             show: ['email', 'phoneNumber', 'city', 'country'],
@@ -22,12 +25,92 @@ RebillyInstruments.mount({
             planId: 'premium-membership',
             quantity: 1
         },
+    ],
+    addons: [
+        {
+            planId: 'personalized-travel-plan',
+            quantity: 1
+        }
+    ],
+    bumpOffer: [
+        {
+            planId: 'platinum-membership',
+            quantity: 1
+        }
     ]
 });
-// Optional
+
+const button = document.getElementById('btn');
+
+button.addEventListener('click', (e) => {
+    if (button.textContent === 'change to yearly plan') {
+        button.textContent = 'change to monthly plan';
+        const newConfig = {
+            items: [
+                {
+                    planId: 'yearly-premium-membership',
+                    quantity: 1
+                }
+            ],
+            bumpOffer: [
+                {
+                    planId: 'yearly-platinum-membership',
+                    quantity: 1
+                }
+            ]
+        };
+        try {
+            RebillyInstruments.update(newConfig);
+        } catch (error) {
+            console.log('error: ', error);
+        }
+    } else {
+        button.textContent = 'change to yearly plan';
+        const newConfig = {
+            items: [
+                {
+                    planId: 'premium-membership',
+                    quantity: 1
+                }
+            ],
+            bumpOffer: [
+                {
+                    planId: 'platinum-membership',
+                    quantity: 1
+                }
+            ]
+        };
+        try {
+            RebillyInstruments.update(newConfig);
+        } catch (error) {
+            console.log('error: ', error);
+        }
+    }
+})
+
 RebillyInstruments.on('instrument-ready', (instrument) => {
-    console.info('instrument-ready', instrument);
+    const {billingAddress, _raw} = instrument;
+    const form = document.querySelector('[data-rebilly-instruments="form"]');
+    const confirmationScreen = document.querySelector('.rebilly-instruments-custom-confirmation');
+    form.classList.add('display-none');
+    confirmationScreen.classList.remove('display-none');
+
+    const confirm = document.querySelector('#confirm');
+    confirm.addEventListener('click', makePurchase);
+
+    async function makePurchase() {
+        const agree = document.querySelector('#confirmation-terms');
+
+        if (agree.checked) {
+            form.classList.remove('display-none');
+            confirmationScreen.classList.add('display-none');
+            RebillyInstruments.show('confirmation', instrument);
+        } else {
+            document.querySelector('#confirmation-terms-error').innerHTML = '<br/>Please agree with terms and conditions';
+        }
+    }
 });
+// Optional
 RebillyInstruments.on('purchase-completed', (purchase) => {
     console.info('purchase-completed', purchase);
 });
